b737e0fabed20eddf44423c2f4b46fda2f53aa8a
FaroqAlkatheri <153449802+FaroqAlkatheri@users.noreply.github.com>
2024-05-17 16:29:19 +0000

    Service1

---
+++ nodeApp/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const uri = 'mongodb+srv://abdulrahmansu10:x1oIn3AtyHyaCfVz@cluster0.oi9efuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const databaseName = 'security_incidents';
+const collectionName = 'incidents';
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    process.exit(1);
+  }
+}
+
+app.get('/devices/:deviceId/incidents/:incidentType', async (req, res) => {
+  try {
+    const { deviceId, incidentType } = req.params;
+    
+    const database = client.db(databaseName);
+    const collection = database.collection(collectionName);
+    
+    const query = {
+      deviceId: deviceId,
+      'report.detected_objects.class_name': incidentType
+    };
+    
+    const incidents = await collection.find(query).toArray();
+    
+    res.json(incidents);
+  } catch (error) {
+    console.error('Error retrieving incidents:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+async function startServer() {
+  await connectToDatabase();
+  
+  const port = 8080;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+startServer();
